4b4a7a15bc7ddb1a17bf9825af677b473d30a434
Leonid Shalupov <shalupov@diverse.org.ru>
2009-06-26 20:16:28 +0400

    command line options for schema generator

--- sqlbuilder/SqlBuilder.SchemaGenerator/src/SchemaGeneratorProgram.cs
+++ sqlbuilder/SqlBuilder.SchemaGenerator/src/SchemaGeneratorProgram.cs
@@ -2,26 +2,53 @@
 using System.Data;
 using System.Linq;
 using MySql.Data.MySqlClient;
+using NDesk.Options;
 
-namespace SqlBuilder.SchemaGenerator {
-  internal class SchemaGeneratorProgram {
-    private static void Main(string[] args) {
-      string connectionString = "Server=localhost; Database=prod; User ID=root; Password=; charset=utf8;";
-      string schema = "prod";
-      string nameSpace = "Mnemoply.Schema";
+namespace SqlBuilder.SchemaGenerator
+{
+  internal class SchemaGeneratorProgram
+  {
+    private static void Main(string[] args)
+    {
+      string connectionString = null;
+      string schema = null;
+      string nameSpace = null;
 
-      using (IDbConnection connection = new MySqlConnection(connectionString)) {
+      var p = new OptionSet
+                {
+                  {"h|?|help", v => ShowHelpAndExit()},
+                  {"c|connection", v => connectionString = v},
+                  {"s|schema", v => schema = v},
+                  {"n|namespace", v => nameSpace = v},
+                };
+      p.Parse(args);
+
+      if (connectionString == null || schema == null || nameSpace == null)
+      {
+        Console.Error.WriteLine("You must specify all arguments");
+        ShowHelpAndExit();
+      }
+
+      using (IDbConnection connection = new MySqlConnection(connectionString))
+      {
         connection.Open();
 
         var generator = new Generator(Console.Out, new NameConverter());
         var catalog = new Catalogs(connection);
 
         generator.WriteHeader(nameSpace);
-        foreach (string table in catalog.GetTables(schema).ToList()) {
+        foreach (string table in catalog.GetTables(schema).ToList())
+        {
           generator.WriteTableSchema(table, catalog.GetTableColumns(schema, table));
         }
         generator.WriteFooter();
       }
+    }
+
+    private static void ShowHelpAndExit()
+    {
+      Console.Error.WriteLine("Usage: SqlBuilder.SchemaGenerator.exe -c <connectionString> -s <database> -n <namespace>");
+      Environment.Exit(1);
     }
   }
 }
