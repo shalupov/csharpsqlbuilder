d6ec596684b1044e40ef0b8d4d1383bf52a695a1
ignatov <ignatov@localhost>
2009-07-01 14:25:57 +0400

    Now command line options works.

--- sqlbuilder/SqlBuilder.SchemaGenerator/src/SchemaGeneratorProgram.cs
+++ sqlbuilder/SqlBuilder.SchemaGenerator/src/SchemaGeneratorProgram.cs
@@ -2,13 +2,27 @@
 using System.Data;
 using System.Linq;
 using MySql.Data.MySqlClient;
+using NDesk.Options;
 
 namespace SqlBuilder.SchemaGenerator {
   internal class SchemaGeneratorProgram {
     private static void Main(string[] args) {
-      const string connectionString = "Server=127.0.0.1; Database=prod; User ID=root; Password=; charset=utf8;";
-      const string schema = "prod";
-      const string nameSpace = "Mnemoply.Schema";
+      string connectionString = null;
+      string schema = null;
+      string nameSpace = null;
+
+      var p = new OptionSet {
+                              {"h|?|help", v => ShowHelpAndExit()},
+                              {"c|connection=", v => connectionString = v},
+                              {"s|schema=", v => schema = v},
+                              {"n|namespace=", v => nameSpace = v},
+                            };
+      p.Parse(args);
+
+      if (connectionString == null || schema == null || nameSpace == null) {
+        Console.Error.WriteLine("You must specify all arguments");
+        ShowHelpAndExit();
+      }
 
       using (IDbConnection connection = new MySqlConnection(connectionString)) {
         connection.Open();
@@ -17,11 +31,15 @@ namespace SqlBuilder.SchemaGenerator {
         var catalog = new Catalogs(connection);
 
         generator.WriteHeader(nameSpace);
-        foreach (string table in catalog.GetTables(schema).ToList()) {
+        foreach (string table in catalog.GetTables(schema).ToList())
           generator.WriteTableSchema(schema, table, catalog.GetTableColumns(schema, table));
-        }
         generator.WriteFooter();
       }
+    }
+
+    private static void ShowHelpAndExit() {
+      Console.Error.WriteLine("Usage: SqlBuilder.SchemaGenerator.exe -c <connectionString> -s <database> -n <namespace>");
+      Environment.Exit(1);
     }
   }
 }
