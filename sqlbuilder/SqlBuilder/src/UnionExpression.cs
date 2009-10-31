e40074a35980dfd2216c1ef3581700ed1fdeaf48
sergey <sergey@ignatov.spb.su>
2009-11-01 01:39:51 +0300

    added union expression and devide operator

---
+++ sqlbuilder/SqlBuilder/src/UnionExpression.cs
@@ -0,0 +1,26 @@
+﻿namespace SqlBuilder {
+  public class UnionExpression : Expression {
+    private readonly SelectStatement myFirstSelectStatement;
+    private readonly SelectStatement mySecondSelectStatement;
+
+    public UnionExpression(SelectStatement s1, SelectStatement s2) {
+      myFirstSelectStatement = s1;
+      mySecondSelectStatement = s2;
+    }
+
+    public override Expression Clone() {
+      return new UnionExpression(myFirstSelectStatement, mySecondSelectStatement);
+    }
+
+    public override void Accept(IVisitor visitor) {
+      visitor.Visit(this);
+    }
+
+    public override string ToSQL() {
+      return ("UNION " +
+              myFirstSelectStatement.ToSQL().SurroundWithBrackets() +
+              mySecondSelectStatement.ToSQL().SurroundWithBrackets()
+             ).SurroundWithBrackets();
+    }
+  }
+}
