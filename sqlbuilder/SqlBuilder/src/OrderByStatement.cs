254d2a428e2b742e01a507b7d91c8f69bb6eab39
sergey <sergey@ignatov.spb.su>
2009-11-01 23:49:45 +0300

    added ORDER BY for UNION statement

---
+++ sqlbuilder/SqlBuilder/src/OrderByStatement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SqlBuilder {
+  public class OrderByStatement {
+    public SqlColumn Column { get; set; }
+    public OrderByType Type { get; set; }
+
+    private static string OrderByType2String(OrderByType type) {
+      switch (type) {
+        case OrderByType.Ascending:
+          return "ASC";
+        case OrderByType.Descending:
+          return "DESC";
+        default:
+          throw new ApplicationException("Uncovered order by type: " + type);
+      }
+    }
+
+    public string ToSQL() {
+      return Column.ToSQL() + " "
+             + OrderByType2String(Type);
+    }
+  }
+}
